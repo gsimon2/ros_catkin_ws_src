e7658e1fcc7f7e379a2a69b779e6c5dd3f429b49
Glen Simon <simongle@msu.edu>
2017-06-28 12:46:14 -0400

    Finished writing the update VMs script

--- python_scripts/update_VMs.py
+++ python_scripts/update_VMs.py
@@ -4,19 +4,29 @@ import os
 
 parser = argparse.ArgumentParser(description='Script used for updating the ros_catkin_ws code on all of the robo VMs from the github repo')
 #parser.add_argument('-r', '--remote', action='store_true', help='Use when not on MSU Engineering network. SSH\'s into arctic server before going to robo servers')
-parser.add_argument('-d', '--debug', action='store_true', help='Print extra output to terminal, spawn subprocesses in xterm for seperated process outputs')
-parser.add_argument('-p', '--password', type=str, help='Users password on remote machine. This is a required parameter')
+#parser.add_argument('-d', '--debug', action='store_true', help='Print extra output to terminal, spawn subprocesses in xterm for seperated process outputs')
+#parser.add_argument('-p', '--password', type=str, help='Users password on remote machine. This is a required parameter')
 args= parser.parse_args()
 
 print('Starting update scripts on robo nodes...')
 
-current_vm = 2
-cmd_str = """xterm -title 'Update Script" -hold -e '
-			sshpass -p '{}' ssh -X robo1vm{}.cse.msu.edu;
-			echo \"Login Successful!\"
-		'&""".format(args.password, current_vm)
+current_vm = 1
+while current_vm <= 4:
+	cmds = """echo 'Forcing all ros_catkin_ws/src code to match Github';
+		cd simulation/ros_catkin_ws/src/;
+		git fetch --all;
+		git reset --hard origin/master;
+		git log -1;
+		exec bash
+		"""
+	cmd_str = 'xterm -title "Connection to robo1vm{}" -hold -e ssh -t -X robo1vm{}.cse.msu.edu "{}"&'.format(current_vm,current_vm,cmds)
+	os.system(cmd_str)
+	current_vm += 1
 
-mavproxy = subprocess.Popen(cmd_str, stdout=subprocess.PIPE, shell=True)
-#os.system(cmd_str)
+print('Script finished! \n')
 
-print('Script finished!')
+print('Press enter to close all xterm windows and close this script...')
+_ = raw_input()
+cmd_str = "pkill xterm"
+os.system(cmd_str)
+
